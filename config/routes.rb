596f5eca46d7e41df0f19064a1b500a42fa2a4d7
Frappe811 <monichan811@gmail.com>
2017-04-16 22:54:00 +0700

    Sau khi user dang nhap, 'log in' => 'log out', 'sign in' => 'upload'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    omiauth_callbacks: 'users/omiauth_callbacks',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
   }
   root 'posts#index'
   devise_scope :user do
